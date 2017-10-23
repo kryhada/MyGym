4a769bf3acd1745d19756edeedc3111b7e828107
Pawlik <ghost131@users.noreply.github.com>
2017-10-23 12:06:57 +0200

    dupa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Czarna pyta nie umyta!!!");
+=======
+		System.out.println("Hello co robimy!!!");
+=======
+		System.out.println("myje pyte");
+>>>>>>> whats up
 	}
 }

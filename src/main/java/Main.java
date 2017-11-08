ac2559471b806916db2bd31458006df8b18d3db3
kryhada <kr_hadas@hotmail.com>
2017-11-08 22:22:01 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,6 @@
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Czarna pyta nie umyta!!!");
-=======
-		System.out.println("Hello co robimy!!!");
-=======
-		System.out.println("myje pyte");
->>>>>>> whats up
+		//testy
 	}
 }
